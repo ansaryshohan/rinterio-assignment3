62899fc2dbecb3d900a7263e3960b7f5d139f99e
ansaryshohan <ansary2083@gmail.com>
2024-08-22 10:50:10 +0600

    project setup and header and hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        "green-btn": "#ABEF5F",
+        "light-green-btn": "#ABEF5F80",
+        "primary-black-text": "#1C1C1C",
+        "secondary-gray-text": "#737373",
+        "gray-primary-bg": "#e9e9e9",
+      },
+    },
+  },
+};
